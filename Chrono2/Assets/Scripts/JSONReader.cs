09baf98c5a81952cc7022aaa6a090c53252907ea
Henrique Machado Gonçalves <henriquemg93@gmail.com>
2022-08-07 19:52:44 -0300

    Fix Json Webgl issue(?)

--- Chrono2/Assets/Scripts/JSONReader.cs
+++ Chrono2/Assets/Scripts/JSONReader.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 using SimpleJSON;
 public class JSONReader : MonoBehaviour
 {
+    string jsonString;
     public string path;
     public TextAsset jsonFile;
     public JSONNode data;
     public void Load() 
     {
-        path="./Assets/Scripts/TimeLine.json";
-        string jsonString = File.ReadAllText(path); 
+    //     path="./Assets/Scripts/TimeLine.json";
+    //     string jsonString = File.ReadAllText(path); 
+        jsonString = "{\"Items\":[{\"name\":\"Egg\" ,   \"time\":  \"-340000000\"   },{\"name\":\"Fire\" ,  \"time\":  \"-300000\"   },{\"name\":\"Castle\", \"time\":  \"-3000\"   },{\"name\":\"Animal Domestication\", \"time\":  \"-8000\"   },{\"name\":\"Apocalypse\" , \"time\":  \"1000000\"   },{\"name\":\"Spear\" , \"time\":  \"-500000\"   },{\"name\":\"Beer\" ,  \"time\":  \"-7000\"   },{\"name\":\"Wine\" ,  \"time\":  \"-8500\"   },{\"name\":\"Cheese\", \"time\":  \"-8000\"   },{\"name\":\"Bread\" , \"time\":  \"-8000\"   },{\"name\":\"Fabric\" ,\"time\":  \"-5000\"   },{\"name\":\"Leather\",\"time\":  \"-1500\"   },{\"name\":\"Paper\" , \"time\":  \"220\"   },{\"name\":\"Coin\" ,  \"time\":  \"-600\"   },{\"name\":\"Candle\" ,\"time\":  \"-200\"   },{\"name\":\"Lantern\",\"time\":  \"-230\"   },{\"name\":\"Map\" ,   \"time\":  \"-600\"   },{\"name\":\"Axe\" ,   \"time\":  \"-6000\"   },{\"name\":\"Pickaxe\",\"time\":  \"-2700\"   },{\"name\":\"Sword\" , \"time\":  \"-3300\"   },{\"name\":\"Shield\" ,\"time\":  \"-1300\"   },{\"name\":\"Chicken\",\"time\":  \"-58000\"   }]}";
         data = JSON.Parse(jsonString);
         foreach(JSONNode item in data["Items"])
         {
